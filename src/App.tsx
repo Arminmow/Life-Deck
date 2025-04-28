cef0ffcf31f9a49ac7ba32f4da7f94b3e7ab8680
Armin <arminmow@gmail.com>
2025-04-28 22:42:18 +0330

    Installed Framer Motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { motion } from "framer-motion";
 
 function App() {
   return (
-    <>
-      <Button>Hi!</Button>
+    <>  
+      <motion.div 
+        className="inline-block"
+        animate= {{scale: [1,2,1]}}
+        transition={{repeat: Infinity, duration: 1.5 , ease: "linear"}}
+      >
+        <Button>ShadCN + Framer Motion</Button>
+      </motion.div>
     </>
   );
 }
